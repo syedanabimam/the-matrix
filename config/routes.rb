6dfd26a020ac4300a13b8b5bc6bf2b59e8b41f29
Syed Anab Imam <syedanabimam@gmail.com>
2017-09-01 06:05:53 +0500

    Architect Devise added and Tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :architects, path_prefix: 'my'
+  resources :architects, only: %i[index show] do
+    # resource :architect_profile
+    # resource :sheeple
+    # collection do
+    #   post :create_agents
+    #   get :get_agents
+    #   post :change_agents
+    #   get :delete_agents
+    #   get :transform_sheeples_to_agents
+    #
+    #   post :create_sentinels
+    #   get :get_sentinels
+    #   post :change_sentinels
+    #   get :delete_agents
+    #   get :destroy_resistance_ships
+    #
+    #   post :create_resistance_members
+    #   get :get_resistance_members
+    #   post :change_resistance_members
+    #   get :delete_resistance_members
+    #   get :resistance_transform_to_new_identity
+    #
+    #   post :create_resistance_ships
+    #   get :get_resistance_ships
+    #   post :change_resistance_ships
+    #   get :delete_resistance_ships
+    #   get :destroy_sentinels
+    #
+    #   get :the_one
+    #   get :the_one_destroy_agents
+    #   get :the_one_flies
+    # end
+  end
+
+  root to: 'home#index'
 end
---
+++ spec/factories/architects.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :architect do
+    
+  end
+end
